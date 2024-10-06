8a47468363a76f7868a353ddc791044d0c6e9d28
rishav0821 <rishav.18271@stu.upes.ac.in>
2024-10-06 23:23:45 +0530

    new

--- class Work/mymodule.py
+++ class Work/mymodule.py
@@ -5,3 +5,12 @@ def myadd(a,b):
 
 def mydiff(a,b):
     return a-b
+
+if __name__ =="__main__":
+    print("executed as main file")
+    a=1
+    b=2
+    print(f"addition of{a} and {b} ={myadd(a,b)}")
+
+else:
+    print("imported as module")
