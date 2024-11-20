358e68d127d5ef12acc95fc2942dfab6a65e215d
rishav0821 <rishavkr696@gmail.com>
2024-11-20 20:03:29 +0530

    nodes

---
+++ class Work/Nodes/main.py
@@ -0,0 +1,29 @@
+# we have x,y coordinates of following we need to find distance between nodes and store the result
+O = (0,0)
+P = (1,2)
+Q = (1,4)
+R = (4,6)
+S = (7,8)
+
+# OR
+O1 = {x:0,y:0}
+P1 = {x:1,y:2}
+P2 = {x:1,y:4}
+P3 = {x:4,y:6}
+P4 = {x:7,y:8}
+# OR
+Points = [ {x:0,y:0}, {x:1,y:2}, {x:1,y:4}, {x:4,y:6}, {x:7,y:8} ]
+
+def distance(x,y):
+    return (x**2+y**2)**(1/2)
+
+def R():
+    pass
+
+def Theta():
+    pass
+
+def Move():
+    pass
+
+ 
